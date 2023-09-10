59800ec7584e1942459c9ecf54348ea344a52c6c
itayz1e <itayz1e@gmail.com>
2023-09-10 23:40:20 +0300

    restyle clinet and wip on form

--- client/src/components/FetchFluidSpeed.tsx
+++ client/src/components/FetchFluidSpeed.tsx
@@ -1,14 +1,14 @@
-import '../style/fetchFluidSpeed.scss'
+// import '../style/fetchFluidSpeed.scss'
 
-function FetchFluidSpeed() {
-  return (
-    <div>
-      <div className='divResult'>
-        <button className='btnResult'>Fetch Fluid Speed</button>
-      </div>
-      <p className='result'>get a result...</p>
-    </div>
-  )
-}
+// function FetchFluidSpeed() {
+//   return (
+//     <div>
+//       <div className='divResult'>
+//         <button className='btnResult'>Fetch Fluid Speed</button>
+//       </div>
+//       <p className='result'>get a result...</p>
+//     </div>
+//   )
+// }
 
-export default FetchFluidSpeed
+// export default FetchFluidSpeed
--- client/src/components/ScrapeForBarcodes.tsx
+++ client/src/components/ScrapeForBarcodes.tsx
@@ -1,44 +1,44 @@
-import "../style/scrapeForBarcodes.scss";
+// import "../style/scrapeForBarcodes.scss";
 
-function ScrapeForBarcodes() {
-  return (
-    <div className="container">
-      <div className="container__image">
-        <p>500ML</p>
-        <button>
-          <img
-            src="https://barcode-list.com/barcodeImage.php?barcode=5099873089712"
-            alt="500"
-            className="image"
-          />
-        </button>
-      </div>
-      <div className="container__image">
-        <p>750ML</p>
-        <button>
-          <img
-            src="https://barcode-list.com/barcodeImage.php?barcode=5099873089712"
-            alt="750"
-            className="image"
-          />
-        </button>
-      </div>
-      <div className="container__image">
-        <p>1000ML</p>
-        <button>
-          <img
-            src="https://barcode-list.com/barcodeImage.php?barcode=5099873089712"
-            alt="1000"
-            className="image"
-          />
-        </button>
-      </div>
-      <div>
-      <input type="checkbox" className="click" />
-      <label htmlFor="bien" className="confirmed">Please select an image and confirm</label>
-    </div>
-    </div>
-  );
-}
+// function ScrapeForBarcodes() {
+//   return (
+//     <div className="container">
+//       <div className="container__image">
+//         <p>500ML</p>
+//         <button>
+//           <img
+//             src="https://barcode-list.com/barcodeImage.php?barcode=5099873089712"
+//             alt="500"
+//             className="image"
+//           />
+//         </button>
+//       </div>
+//       <div className="container__image">
+//         <p>750ML</p>
+//         <button>
+//           <img
+//             src="https://barcode-list.com/barcodeImage.php?barcode=5099873089712"
+//             alt="750"
+//             className="image"
+//           />
+//         </button>
+//       </div>
+//       <div className="container__image">
+//         <p>1000ML</p>
+//         <button>
+//           <img
+//             src="https://barcode-list.com/barcodeImage.php?barcode=5099873089712"
+//             alt="1000"
+//             className="image"
+//           />
+//         </button>
+//       </div>
+//       <div>
+//       <input type="checkbox" className="click" />
+//       <label htmlFor="bien" className="confirmed">Please select an image and confirm</label>
+//     </div>
+//     </div>
+//   );
+// }
 
-export default ScrapeForBarcodes;
+// export default ScrapeForBarcodes;
